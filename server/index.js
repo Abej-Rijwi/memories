dfc55feb3983f968adbddb92944f885c5fe16976
Abej-Rijwi <abejrijwicareer@gmail.com>
2025-05-11 16:13:13 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -46,6 +46,10 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Memories API!");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
